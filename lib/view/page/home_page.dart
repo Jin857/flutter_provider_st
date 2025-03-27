342a8a00306690106733e985cd31701b745c9e1d
Jin857 <jinnnnnnnn857@outlook.com>
2025-03-27 09:42:25 +0800

    添加资源

--- lib/view/page/home_page.dart
+++ lib/view/page/home_page.dart
@@ -160,7 +160,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           title: title,
                           subtitle: subtitle,
                           onClick: () async {
-                            await Future.delayed(Duration(seconds: 1));
+                            await Future.delayed(const Duration(seconds: 1));
                           },
                         );
                       },
